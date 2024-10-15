90898facff6289a721434d7ac68cf6e12ee6e772
JAKUAN-AHMED <jakuanultimate777@gmail.com>
2024-10-16 01:07:21 +0600

    updated server side code for deleting item

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 
 const app=express();
@@ -55,6 +55,14 @@ async function run() {
       const email=req.query.email;
       const query={email:email};
       const result=await cartsCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    //delete cart
+    app.delete('/carts/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={_id:new ObjectId(id)};
+      const result=await cartsCollection.deleteOne(query);
       res.send(result);
     })
 
